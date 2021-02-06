5d8b245341884eeef8cf8c7f9f716a6da73fa456
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-02-06 20:54:49 +0500

    Update index.js

--- react-hooks-props-component/src/index.js
+++ react-hooks-props-component/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Reddit() {
+  const [posts, setPosts] = useState([]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+  
+        setPosts(newPosts);
+      });
+  }, []);
+
+  return(
+     <div>
+       <h1>/r/reactjs </h1>
+       <ul>
+         {posts.map(post => (
+           <li key = {post.id}>{post.title}</li>
+         ))}
+       </ul>
+     </div>
+  );
+}
+
+ReactDOM.render(<Reddit />, document.getElementById('root'));
